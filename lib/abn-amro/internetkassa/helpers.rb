3e09bb1b4a4248863486e594400d3fbadeecc933
Reinier de Lange <r.j.delange@nedforce.nl>
2014-01-02 17:47:42 +0100

    Internetkassa link generator

--- lib/abn-amro/internetkassa/helpers.rb
+++ lib/abn-amro/internetkassa/helpers.rb
@@ -3,10 +3,16 @@ module AbnAmro
     module Helpers
       def internetkassa_form_tag(internetkassa_instance, &block)
         content_tag :form, :action => AbnAmro::Internetkassa.service_url, :method => 'post' do
-          result = internetkassa_instance.data.map { |name, value| hidden_field_tag(name, value, :id => nil) }          
+          result = internetkassa_instance.data.map { |name, value| hidden_field_tag(name, value, :id => nil) }
           result << (block_given? ? capture(&block) : submit_tag('iDeal'))
           result.join("\n").html_safe
         end
+      end
+
+      def internetkassa_link_to internetkassa_instance, text = 'iDeal'
+        uri = Addressable::URI.parse AbnAmro::Internetkassa.service_url
+        uri.query_values = internetkassa_instance.data
+        link_to text, uri.to_s
       end
     end
   end
